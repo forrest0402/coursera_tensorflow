35ba41cc3fe2e9e0e439dfcc00cc15e5ac97932a
forrest <forrest0402@hotmail.com>
2019-08-27 00:25:04 +0800

    add cnn

---
+++ introduction_to_cnn.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author: xiezizhe
+@Date: 2019/8/24 4:50 PM
+"""
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+print(tf.__version__)
+if __name__ == '__main__':
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    print(len(x_train))
+    print(len(x_test))
+    print("train_data shape:", x_train.shape, "train_label shape:", y_train.shape)
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+    x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+    print("train_data shape:", x_train.shape, "test_data shape:", x_test.shape)
+
+
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if logs.get('acc') > 0.998:
+                print("\nReached 998% accuracy so cancelling training!")
+                self.model.stop_training = True
+
+
+    callbacks = myCallback()
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(kernel_size=3, filters=64, padding="same", activation=tf.nn.relu,
+                               input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        tf.keras.layers.Conv2D(kernel_size=5, filters=64, padding="same", activation=tf.nn.relu),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax),
+    ])
+
+    model.summary()
+
+    model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=20, callbacks=[callbacks])
+    print("evaluate")
+    print(model.evaluate(x_test, y_test))
+    print("hello")
