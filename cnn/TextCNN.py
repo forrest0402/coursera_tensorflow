5faca2bba81420538c0a568a018fc3d3e481d47a
forrest <forrest0402@hotmail.com>
2019-10-30 02:55:56 +0800

    fix bugs

--- cnn/TextCNN.py
+++ cnn/TextCNN.py
@@ -67,52 +67,89 @@ if __name__ == '__main__':
     # sentences = ["i love you", "he loves me", "she likes baseball", "i hate you", "sorry for that", "this is awful"]
     # labels = [1, 1, 1, 0, 0, 0]  # 1 is good, 0 is not good.
 
-    imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
-    train_data, test_data = imdb['train'], imdb['test']
+    # imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+    # train_data, test_data = imdb['train'], imdb['test']
+    #
+    # training_sentences = []
+    # training_labels = []
+    #
+    # testing_sentences = []
+    # testing_labels = []
+    #
+    # for (x, y) in train_data:
+    #     training_sentences.append(str(x.numpy()))
+    #     training_labels.append(str(y.numpy()))
+    #
+    # for (x, y) in test_data:
+    #     testing_sentences.append(str(x.numpy()))
+    #     testing_labels.append(str(y.numpy()))
+    #
+    # training_labels = np.array(training_labels)
+    # testing_labels = np.array(testing_labels)
+    #
+    # vocab_size = 10000
+    # embedding_dim = 16
+    # max_length = 120
+    # trunc_type = 'post'
+    # oov_tok = "<OOV>"
+    #
+    # tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    # tokenizer.fit_on_texts(training_sentences)
+    # word_index = tokenizer.word_index
+    #
+    # train_sequences = tokenizer.texts_to_sequences(training_sentences)
+    # train_padded_seq = tf.keras.preprocessing.sequence.pad_sequences(train_sequences, maxlen=max_length,
+    #                                                                  truncating=trunc_type)
+    #
+    # test_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    # test_padded_seq = tf.keras.preprocessing.sequence.pad_sequences(test_sequences, maxlen=max_length,
+    #                                                                 truncating=trunc_type)
 
-    training_sentences = []
-    training_labels = []
+    embedding_dim = 64
+    max_len = 64
+    max_feature = 2048
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(num_words=max_feature)
+    x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=max_len, padding="post", truncating="post")
+    x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=max_len, padding="post", truncating="post")
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
 
-    testing_sentences = []
-    testing_labels = []
+    model = TextCNN(vocab_size=max_feature,
+                    max_len=max_len,
+                    emb_dim=embedding_dim,
+                    filter_depth=100,
+                    output_size=8196,
+                    task_output=tf.keras.layers.Dense(1, activation='sigmoid'))
 
-    for (x, y) in train_data:
-        training_sentences.append(str(x.numpy()))
-        training_labels.append(str(y.numpy()))
+    loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
+    optimizer = tf.keras.optimizers.Adam(0.001)
+    dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1000).batch(32)
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(1024)
+    for epoch in range(1):
+        losses = []
+        for (batch, (x, y)) in enumerate(dataset):
+            with tf.GradientTape() as tape:
+                predictions = model(x, training=True)
+                loss = tf.reduce_mean(loss_object(y, predictions))
+            losses.append(loss.numpy())
+            if batch % 100 == 0:
+                print("{}/{} - loss: {}".format(batch, len(x_train) / 32, np.mean(losses)))
+            gradients = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-    for (x, y) in test_data:
-        testing_sentences.append(str(x.numpy()))
-        testing_labels.append(str(y.numpy()))
+        accu = []
+        for (batch, (x, y)) in enumerate(test_dataset):
+            predictions = model(x, training=False)
+            print(tf.reduce_sum(tf.cast(tf.equal(predictions, tf.cast(y, dtype=tf.float32)), dtype=tf.int32)).numpy())
 
-    training_labels = np.array(training_labels)
-    testing_labels = np.array(testing_labels)
-
-    vocab_size = 10000
-    embedding_dim = 16
-    max_length = 120
-    trunc_type = 'post'
-    oov_tok = "<OOV>"
-
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-    tokenizer.fit_on_texts(training_sentences)
-    word_index = tokenizer.word_index
-
-    train_sequences = tokenizer.texts_to_sequences(training_sentences)
-    train_padded_seq = tf.keras.preprocessing.sequence.pad_sequences(train_sequences, maxlen=max_length,
-                                                                     truncating=trunc_type)
-
-    test_sequences = tokenizer.texts_to_sequences(testing_sentences)
-    test_padded_seq = tf.keras.preprocessing.sequence.pad_sequences(test_sequences, maxlen=max_length,
-                                                                    truncating=trunc_type)
-
-    textCnn = TextCNN(vocab_size=vocab_size,
-                      max_len=max_length,
-                      emb_dim=embedding_dim,
-                      filter_depth=100,
-                      output_size=8196,
-                      task_output=tf.keras.layers.Dense(1, activation='sigmoid'))
-
-    model = textCnn
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    num_epochs = 10
-    model.fit(train_padded_seq, training_labels, epochs=num_epochs, validation_data=(test_padded_seq, testing_labels))
+    # textCnn = TextCNN(vocab_size=vocab_size,
+    #                   max_len=max_length,
+    #                   emb_dim=embedding_dim,
+    #                   filter_depth=100,
+    #                   output_size=8196,
+    #                   task_output=tf.keras.layers.Dense(1, activation='sigmoid'))
+    #
+    # model = textCnn
+    # model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # num_epochs = 10
+    # model.fit(train_padded_seq, training_labels, epochs=num_epochs, validation_data=(test_padded_seq, testing_labels))
